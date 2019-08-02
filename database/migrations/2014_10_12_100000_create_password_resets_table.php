2ec21cde3e2ed2aa67c69579a61d2350756ad3e1
Jorge Garcell <jagarcell@gmail.com>
2019-08-02 16:47:03 -0400

    email and database adjustment for new host

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,7 +14,7 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
+            $table->string('email')->index()->collation('utf8_bin');;
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
