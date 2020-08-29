bd94d7e1a88d9acb956a4c908d866ce01db869d5
Jorge Garcell <jagarcell@gmail.com>
2020-08-29 12:18:23 -0400

    c

--- public/js/placeanorder.js
+++ public/js/placeanorder.js
@@ -300,6 +300,7 @@ function selectChanged(checkB) {
 		var row = checkbox.parentNode.parentNode
 
 		var instock = $('#instock_' + row.id).textContent
+		console.log(instock)
 
 		var price = row.children['price_' + row.id].innerHTML
 
