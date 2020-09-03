ae39574eed1496da4377ef4298c8f3d2ccc1d9d2
Jorge Garcell <jagarcell@gmail.com>
2020-09-02 20:37:40 -0400

    order test

--- app/Orders.php
+++ app/Orders.php
@@ -468,6 +468,7 @@ class Orders extends Model
         $authUser = Auth::user();
         if($result['status'] == 'ok'){
             $order = $result['order'];
+            dd($order);
             if($authUser->id == $order->user_id || $authUser->type == 'admin'){
                 $order->specialinstructionsreadonly = "";
                 // IF THE ORDER IS FOR DELIVERY ...
