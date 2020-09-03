7d6a2771ac499a5f243362e2123f13089f71cf38
Jorge Garcell <jagarcell@gmail.com>
2020-09-02 20:52:15 -0400

    Revert "c"
    This reverts commit 7423f090ac0f2c5d66b7dde832769180a55e419c.

--- app/Orders.php
+++ app/Orders.php
@@ -468,7 +468,7 @@ class Orders extends Model
         $authUser = Auth::user();
         if($result['status'] == 'ok'){
             $order = $result['order'];
-            if(true){
+            if($authUser->id == $order->user_id){
                 $order->specialinstructionsreadonly = "";
                 // IF THE ORDER IS FOR DELIVERY ...
             dd($order);
