a255d74098de76bba5b3df4c87ffebeb10e0e3b6
Jorge Garcell <jagarcell@gmail.com>
2020-11-13 13:10:55 -0500

    c

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -236,6 +236,7 @@ function priceChange(element) {
 }
 
 function ofertaChange(element) {
+	console.log("OFERTA")
 	var itemId = element.parentNode.parentNode.id
 	var oferta = element.value
 	if(!Number.parseFloat){
