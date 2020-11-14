ac029a33b24addf8b8a948f9429f8c7d4ad91fcf
Jorge Garcell <jagarcell@gmail.com>
2020-11-13 20:25:45 -0500

    c

--- public/js/placeanorder.js
+++ public/js/placeanorder.js
@@ -49,7 +49,7 @@ $(document).ready(function placeAnOrderReady() {
 	window.setTimeout(function(elements, index){
 		console.log(elements)
 		console.log(index)
-		window.setTimeout(elements, index + 1)
+		window.setTimeout(this(this.elements, this.index + 1), 3000)
 	}, 3000, ofertaNDivs, 0)
 })
 
