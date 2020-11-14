e85c1078854de46e583a48e9db2ba7630d1f8a63
Jorge Garcell <jagarcell@gmail.com>
2020-11-13 19:36:43 -0500

    c

--- public/js/placeanorder.js
+++ public/js/placeanorder.js
@@ -47,7 +47,7 @@ $(document).ready(function placeAnOrderReady() {
 
 	var ofertaNDivs = $(".ofertaNDiv")
 	console.log(ofertaNDivs)
-	console.log(ofertaNDivs[0])
+	console.log(ofertaNDivs[0].attr('opacity'))
 
 })
 
