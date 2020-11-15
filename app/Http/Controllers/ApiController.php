61eb7d113e649238d855fbed255702bf0513fd10
Jorge Garcell <jagarcell@gmail.com>
2020-11-15 12:53:06 -0500

    ApiController.php

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Api;
+
+class ApiController extends Controller
+{
+    //
+    public function Inventory(Request $request)
+    {
+    	# code...
+        return (new Api())->Inventory($request);
+    }
+
+    public function CreateApiKey(Request $request)
+    {
+    	# code...
+    	return (new Api())->CreateApiKey($request);
+    }
+
+
+    public function ApiTest(Request $request)
+    {
+        return (new Api())->ApiTest($request);
+    }
+}
