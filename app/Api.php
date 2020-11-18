652fe2b6ef13d3c3a17ab50a6f3eadcd9747fcc2
Jorge Garcell <jagarcell@gmail.com>
2020-11-17 21:33:16 -0500

    s

--- app/Api.php
+++ app/Api.php
@@ -126,6 +126,6 @@ class Api extends Model
 
     public function PublicInventory($request)
     {
-		return json_decode((new Inventory())->where('id', '>', -1)->get(['name', 'imgpath']), true);
+		return ((new Inventory())->where('id', '>', -1)->get(['name', 'imgpath']), true);
 	}
 }
