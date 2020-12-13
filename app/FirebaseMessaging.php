299a6b49c08dd0e1fb237484bf117b6d2c73397f
Jorge Garcell <jagarcell@gmail.com>
2020-12-13 13:02:08 -0500

    s

--- app/FirebaseMessaging.php
+++ app/FirebaseMessaging.php
@@ -128,10 +128,9 @@ class FirebaseMessaging extends Model
                 break;
             default:
             echo('NOT ALL');
-                $registeredTokens = DB::table('users')->join('firebase_messagings', function($join){
-                    $join->on(
-                        'users.id', '=', 'firebase_messagings.userid')->where('users.type', '=', $to);
-                })->get();
+                $registeredTokens = DB::table('users')
+                    ->join('firebase_messagings', 'users.id', '=', 'firebase_messagings.userid')
+                        ->where('users.type', '=', $to)->get();
                 return ['tokens' => $registeredTokens] ;   
                 foreach($registeredTokens as $key => $registeredToken){
                     array_push($tokens, $registeredToken->fcm_token);
