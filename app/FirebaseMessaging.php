dcccceee6f4b5b5154eb05259ee4271b7921d3c5
Jorge Garcell <jagarcell@gmail.com>
2020-12-13 10:55:23 -0500

    Comments added to AddFcmToken

--- app/FirebaseMessaging.php
+++ app/FirebaseMessaging.php
@@ -43,33 +43,56 @@ class FirebaseMessaging extends Model
         curl_close($ch);
     }
 
+    /*
+    ***
+    *** Parameters:
+    *** @ token
+    *** @ apikey (optional if it is an authenticated user)
+    ***
+    */
     public function AddFcmToken($request)
     {
          # code...
          $token = $request['token'];
+
+         // DEFAULT userId IS -1 
+         // WILL BE USED IF NO apikey IS PROVIDED (NO AUTHENTICATED USER) OR
+         // IF THERE IS NOT AN USER THAT MATCHS THE PROVIDED apikey   
          $userId = -1;
+
+        // IF apikey IS SET IT IS AN AUTHENTICATED USER
          if(isset($request['apikey'])){
+            // GET THE USER FOR THIS apikey
             $apiKey = $request['apikey'];
             $users = (new Users())->where('api_key', $apiKey)->get();
+            // IF THERE IS AN USER FOR THIS apikey ...
             if(count($users) > 0){
+                // ... THEN SET THE userId TO THIS USER'S ID
                 $user = $users[0];
                 $userId = $user->id;
             }
          }
 
+         // GET THE RECORD FOR THE FCM TOKEN
          try {
             $tokens = $this->where('fcm_token', $token)->get();
+            // IF THERE IS NOT A MATCHING RECORD FOR THE TOKEN ...
             if(count($tokens) == 0){
+                // ... CREATE ONE ASSOCIATED TO THE USER ID
                 $this->fcm_token = $token;
                 $this->userid = $userId;
                 $this->save();
             }
             else{
+                // A RECORD WAS FOUND FOR THE FCM TOKEN
+                // LET'S UPDATE THE USER ASSOCIATED TO IT
                 $this->where('fcm_token', $token)->update(['userid' => $userId]);
             }
+            // EEVRYTHING OK
             return ['status' => 'OK'];
         } catch (\Throwable $th) {
-            return ['status' => 'ERROR'];
+            // SOMETHING WENT WRONG
+            return ['status' => 'ERROR', 'message' => $th];
         }
     }
 }
