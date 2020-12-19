ffbcad3c1d121af4aa87f389c6aca6dfcfa3e796
Jorge Garcell <jagarcell@gmail.com>
2020-12-18 20:35:12 -0500

    s

--- public/js/notification.js
+++ public/js/notification.js
@@ -67,6 +67,7 @@ function sendNotification() {
             {
                 $('#nFailure')[0].textContent = results.failure
                 $('.failureMessagesDiv')[0].style = 'display:inline-block;'
+                $('.failureMessagesDiv')[0].scrollIntoView(true)
                 setTimeout(()=>{$('.failureMessagesDiv')[0].style = 'display:none;'}, 5000)
             }
         }
