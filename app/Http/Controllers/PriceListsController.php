819afa2275a0b297c1922ba71895efb8522ba46c
Jorge Garcell <jagarcell@gmail.com>
2021-01-26 19:40:32 -0500

    test

--- app/Http/Controllers/PriceListsController.php
+++ app/Http/Controllers/PriceListsController.php
@@ -12,7 +12,7 @@ class PriceListsController extends Controller
     //
     public function ModifyPrices(Request $request)
     {
-        return (new PriceListHeader())->ModifyPrices($reuqest);
+        return (new PriceListHeader())->ModifyPrices($request);
     }
 
     public function GetPriceListsHeaders(Request $request)
