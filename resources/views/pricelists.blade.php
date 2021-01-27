82906535cf7346bac816f83ca92ff434ffa791d6
Jorge Garcell <jagarcell@gmail.com>
2021-01-27 13:46:35 -0500

    favicon

--- resources/views/pricelists.blade.php
+++ resources/views/pricelists.blade.php
@@ -14,6 +14,7 @@
 	@endsection
 
 	@section('styles')
+	<link rel="shortcut icon" href="/public/favicon.ico"/>
     <link rel="stylesheet" type="text/css" href="public/css/dropzone.css">
     <link rel="stylesheet" type="text/css" href="public/css/pricelists.css">
 	@endsection
