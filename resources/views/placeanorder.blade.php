a44f72d763bcd0473f8ca6522f32dd6c2bb14d57
Jorge Garcell <jagarcell@gmail.com>
2021-03-26 21:51:38 -0500

    c

--- resources/views/placeanorder.blade.php
+++ resources/views/placeanorder.blade.php
@@ -29,7 +29,7 @@
 	<div class="ofertaWrap">
 	<!--div class="ofertaWrap" onclick="specialClick(this)"-->
 		<div class="ofertasDiv">
-			<a href="www.allwebdone.com">
+			<a href="https://www.allwebdone.com">
 			<img src="public/img/logos/Moving.png" class="ofertasBanner">
 			</a>
 		</div>
