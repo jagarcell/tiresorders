b76062b51f4f527f968d92947b09005ea73d5160
Jorge Garcell <jagarcell@gmail.com>
2021-04-19 18:49:10 -0400

    A test

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -47,6 +47,7 @@ $(document).ready(function InventoryReady() {
 	var dropZones = $('.dropzone')
 
 	$.each(dropZones, function(index, dropzone){
+		console.log(Dropzone.options[dropzone.id])
 		Dropzone.options[dropzone.id] = {
 			uploadMultiple : false,
 			dictDefaultMessage : 'Drop An Image Or Click To Search One',
