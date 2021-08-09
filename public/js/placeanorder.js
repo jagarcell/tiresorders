d50282deb7273426f54b9ef2ed54edce9cccff50
Jorge Garcell <jagarcell@gmail.com>
2021-08-09 16:04:24 -0400

    c

--- public/js/placeanorder.js
+++ public/js/placeanorder.js
@@ -75,7 +75,7 @@ $(document).ready(function placeAnOrderReady() {
 				ofertaNDivs[i].style.opacity = 0
 			}
 		}
-		if(ofertaNDivs.length > 0 && i == ofertaNDivs.length){
+		if(ofertaNDivs.length > 0 && i == ofertaNDivs.length-1){
 			$.get('/specials', function(data, status){
 				if(status == 'success'){
 					var ofertasSlideDiv = document.getElementsByClassName('ofertasSlideDiv')[0]
