06b1643d4ebdd85fc3cf67254df1ac8f3f73e730
Jorge Garcell <jagarcell@gmail.com>
2022-02-07 18:50:25 -0500

    commit

--- resources/views/inventory.blade.php
+++ resources/views/inventory.blade.php
@@ -88,9 +88,9 @@
 	</div>
 	<div class="inventoryButtons">
 		<div class="updateInventory"><input id="updateInventory" type="button" class="actionButton updateInventoryButton" value="UPDATE WITH QB" title="SYNCHRONIZE LOCAL INVENTORY WITH QUICKBOOKS"></div>
-		<div class="updateInventory">
-			<div class="importButton">IMPORT CSV</div>
-			<input id="importCSV" type="file" class="actionButton updateInventoryButton importButton" value="IMPORT CSV" title="IMPORT INVENTORY FROM CSV">
+		<div class="updateInventoryWrap">
+			<input id="importCSV" type="button" class="actionButton updateInventoryButton" value="IMPORT CSV" title="IMPORT INVENTORY FROM CSV">
+			<input id="csvFile" type="file" class="actionButton updateInventoryButton"  title="CHOOSE A CSV FILE">
 		</div>
 	</div>
 </div>
