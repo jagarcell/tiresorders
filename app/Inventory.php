39b3e9cc821c0d76ffe74c902a5b1a4a7eed975a
Jorge Garcell <jagarcell@gmail.com>
2022-02-08 11:46:33 -0500

    c

--- app/Inventory.php
+++ app/Inventory.php
@@ -695,7 +695,7 @@ class Inventory extends Model
                             "\\"
                         )
                     ){
-                        if(line > 0){
+                        if($line > 0){
                             dd($row);
                         }
                     };
