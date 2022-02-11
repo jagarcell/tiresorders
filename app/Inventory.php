229b9f5f6eef252590535c714671f5351b509179
Jorge Garcell <jagarcell@gmail.com>
2022-02-10 20:06:32 -0500

    c

--- app/Inventory.php
+++ app/Inventory.php
@@ -719,7 +719,6 @@ class Inventory extends Model
                         )
                     ){
                         if(count($row) != 6){
-                            dd($row);
                             echo "<div style='color: red;
                             font-size: xx-large;cwidth:100%; height: 150px; display:flex; flex-direction:column; justify-content: center;
                             text-align:center;'>THE FILE FORMAT IS INCORRECT</div>";
@@ -818,8 +817,6 @@ class Inventory extends Model
                 }
             } catch (\Throwable $th) {
                 //throw $th;
-                dd($newItem);
-
             }
         }
         return redirect('/inventory');
